446054b1317ff7a6ddd56472947e34e4dc505b35
Aniket <aniketraj28042003@gmail.com>
2025-05-06 10:16:36 +0530

    add: index.ts: added api endpoint for routes

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { corsOptions } from './util/cors.util';
 import { PORT } from './config/env.config';
+import { router } from './routes/index.route';
 
 const app = express();
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', router);
 
 app.get('/', (_req: Request, res: Response) => {
   res.status(200).json({ message: 'Server is alive' });
