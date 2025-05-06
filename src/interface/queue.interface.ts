89bfdb4e0a975689eba1548c51a4b87c8cfc99e2
Aniket <aniketraj28042003@gmail.com>
2025-05-06 19:13:20 +0530

    add: interface: added interfaces for queue and reminder

---
+++ src/interface/queue.interface.ts
@@ -0,0 +1,6 @@
+export interface IQueue {
+  subject: string;
+  body: string;
+  notification_time: Date;
+  email: string;
+}
--- src/interface/reminder.interface.ts
+++ src/interface/reminder.interface.ts
@@ -2,14 +2,14 @@ import { IReminderStatus } from '../types/index.types';
 
 export interface IReminderRequest {
   user_email: string;
-}
-
-export interface IReminder extends IReminderRequest {
-  id: string;
   subject: string;
   body: string;
   status: IReminderStatus;
   notification_time: Date;
+}
+
+export interface IReminder extends IReminderRequest {
+  id: string;
   created_at: Date;
   updated_at: Date;
 }
