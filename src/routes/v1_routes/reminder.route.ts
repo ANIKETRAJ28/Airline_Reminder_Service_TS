0205107cf1402401c1c696b3a0eb59d079409cfb
Aniket <aniketraj28042003@gmail.com>
2025-05-06 19:17:27 +0530

    update: controller | routes: updated create reminder

--- src/routes/v1_routes/reminder.route.ts
+++ src/routes/v1_routes/reminder.route.ts
@@ -5,7 +5,7 @@ export const reminderRouter = Router();
 
 const reminderController = new ReminderController();
 
-reminderRouter.post('/', reminderController.createReminder.bind(reminderController));
+// reminderRouter.post('/', reminderController.createReminder.bind(reminderController));
 reminderRouter.get('/', reminderController.getAllReminders.bind(reminderController));
 reminderRouter.get('/id/:id', reminderController.getReminderById.bind(reminderController));
 reminderRouter.get('/pending', reminderController.getPendingReminders.bind(reminderController));
