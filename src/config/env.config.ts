ece92caf31ac3a5fc9a9e1bb7da0746aeb86a628
Aniket <aniketraj28042003@gmail.com>
2025-05-06 19:11:05 +0530

    add: config: added env configs

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -9,3 +9,7 @@ export const DB_PORT = process.env.DB_PORT;
 export const DB_USER = process.env.DB_USER;
 export const DB_NAME = process.env.DB_NAME;
 export const NODE_ENV = process.env.NODE_ENV;
+export const AIRLINE_MESSAGE_QUEUE_URL = process.env.AIRLINE_MESSAGE_QUEUE_URL as string;
+export const AIRLINE_MESSAGE_QUEUE_NAME = process.env.AIRLINE_MESSAGE_QUEUE_NAME as string;
+export const EMAIL = process.env.EMAIL;
+export const PASS = process.env.PASS;
